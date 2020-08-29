d0dbb9803eb2aab3764adb66de50bf669cf4b319
Johan-Mi <johanmilanov@outlook.com>
2020-08-29 17:34:20 +0200

    -

--- vector.hpp
+++ vector.hpp
@@ -7,7 +7,8 @@ struct Vector2 {
 	T x{}, y{};
 
 	constexpr Vector2(T const x, T const y) noexcept
-		: x(x), y(y) {}
+		: x(x)
+		, y(y) {}
 
 	Vector2 &operator=(Vector2 const &other) noexcept {
 		x = other.x;
@@ -57,10 +58,10 @@ struct Vector2 {
 		return *this;
 	}
 
-	constexpr auto operator<=>(Vector2 const&) const = default;
+	constexpr auto operator<=>(Vector2 const &) const = default;
 
 	constexpr Vector2 operator-() {
-		return { -x, -y };
+		return {-x, -y};
 	}
 
 	constexpr Vector2 operator+() {
@@ -94,41 +95,46 @@ struct Vector2 {
 	constexpr T angle() const noexcept {
 		return std::atan2(y, x);
 	}
+
+	constexpr Vector2 rotated(T const angle) const noexcept {
+		return {std::cos(angle) * x - std::sin(angle) * y,
+				std::sin(angle) * x + std::cos(angle) * y};
+	}
 };
 
 template<class T>
 Vector2<T> operator+(Vector2<T> const &left, Vector2<T> const &right) noexcept {
-	return { left.x + right.x, left.y + right.y };
+	return {left.x + right.x, left.y + right.y};
 }
 
 template<class T>
 Vector2<T> operator-(Vector2<T> const &left, Vector2<T> const &right) noexcept {
-	return { left.x - right.x, left.y - right.y };
+	return {left.x - right.x, left.y - right.y};
 }
 
 template<class T>
 Vector2<T> operator*(Vector2<T> const &left, Vector2<T> const &right) noexcept {
-	return { left.x * right.x, left.y * right.y };
+	return {left.x * right.x, left.y * right.y};
 }
 
 template<class T>
 Vector2<T> operator/(Vector2<T> const &left, Vector2<T> const &right) {
-	return { left.x / right.x, left.y / right.y };
+	return {left.x / right.x, left.y / right.y};
 }
 
 template<class T>
 Vector2<T> operator%(Vector2<T> const &left, Vector2<T> const &right) noexcept {
-	return { left.x % right.x, left.y % right.y };
+	return {left.x % right.x, left.y % right.y};
 }
 
 template<class T>
 Vector2<T> operator*(Vector2<T> const &vec, T const scalar) noexcept {
-	return { vec.x * scalar, vec.y * scalar };
+	return {vec.x * scalar, vec.y * scalar};
 }
 
 template<class T>
 Vector2<T> operator/(Vector2<T> const &vec, T const scalar) {
-	return { vec.x / scalar, vec.y / scalar };
+	return {vec.x / scalar, vec.y / scalar};
 }
 
 template<class T>
@@ -136,7 +142,9 @@ struct Vector3 {
 	T x{}, y{}, z{};
 
 	constexpr Vector3(T const x, T const y, T const z) noexcept
-		: x(x), y(y), z(z) {}
+		: x(x)
+		, y(y)
+		, z(z) {}
 
 	Vector3 &operator=(Vector3 const &other) noexcept {
 		x = other.x;
@@ -194,10 +202,10 @@ struct Vector3 {
 		return *this;
 	}
 
-	constexpr auto operator<=>(Vector3 const&) const = default;
+	constexpr auto operator<=>(Vector3 const &) const = default;
 
 	constexpr Vector3 operator-() {
-		return { -x, -y, -z };
+		return {-x, -y, -z};
 	}
 
 	constexpr Vector3 operator+() {
@@ -225,44 +233,44 @@ struct Vector3 {
 	}
 
 	constexpr Vector3 cross(Vector3 const &other) const noexcept {
-		return { y * other.z - z * other.y, z * other.x - x * other.z,
-			x * other.y - y * other.x };
+		return {y * other.z - z * other.y, z * other.x - x * other.z,
+				x * other.y - y * other.x};
 	}
 };
 
 template<class T>
 Vector3<T> operator+(Vector3<T> const &left, Vector3<T> const &right) noexcept {
-	return { left.x + right.x, left.y + right.y, left.z + right.z };
+	return {left.x + right.x, left.y + right.y, left.z + right.z};
 }
 
 template<class T>
 Vector3<T> operator-(Vector3<T> const &left, Vector3<T> const &right) noexcept {
-	return { left.x - right.x, left.y - right.y, left.z - right.z };
+	return {left.x - right.x, left.y - right.y, left.z - right.z};
 }
 
 template<class T>
 Vector3<T> operator*(Vector3<T> const &left, Vector3<T> const &right) noexcept {
-	return { left.x * right.x, left.y * right.y, left.z * right.z };
+	return {left.x * right.x, left.y * right.y, left.z * right.z};
 }
 
 template<class T>
 Vector3<T> operator/(Vector3<T> const &left, Vector3<T> const &right) {
-	return { left.x / right.x, left.y / right.y, left.z / right.z };
+	return {left.x / right.x, left.y / right.y, left.z / right.z};
 }
 
 template<class T>
 Vector3<T> operator%(Vector3<T> const &left, Vector3<T> const &right) noexcept {
-	return { left.x % right.x, left.y % right.y, left.z % right.z };
+	return {left.x % right.x, left.y % right.y, left.z % right.z};
 }
 
 template<class T>
 Vector3<T> operator*(Vector3<T> const &vec, T const scalar) noexcept {
-	return { vec.x * scalar, vec.y * scalar, vec.z * scalar };
+	return {vec.x * scalar, vec.y * scalar, vec.z * scalar};
 }
 
 template<class T>
 Vector3<T> operator/(Vector3<T> const &vec, T const scalar) {
-	return { vec.x / scalar, vec.y / scalar, vec.z / scalar };
+	return {vec.x / scalar, vec.y / scalar, vec.z / scalar};
 }
 
 template<class T>
@@ -275,22 +283,23 @@ struct PitchYaw {
 	// pi/2, 0 => Up
 
 	constexpr PitchYaw(T const pitch, T const yaw) noexcept
-		: pitch(pitch), yaw(yaw) {}
+		: pitch(pitch)
+		, yaw(yaw) {}
 };
 
 template<class T>
 constexpr Vector2<T> Vector2_from_angle(T const ang) noexcept {
-	return { std::cos(ang), std::sin(ang) };
+	return {std::cos(ang), std::sin(ang)};
 }
 
 template<class T>
 constexpr PitchYaw<T> PitchYaw_from_Vector3(Vector3<T> const &vec) noexcept {
-	return { std::asin(vec.y), std::atan2(vec.z, vec.x) };
+	return {std::asin(vec.y), std::atan2(vec.z, vec.x)};
 }
 
 template<class T>
 constexpr Vector3<T> Vector3_from_PitchYaw(PitchYaw<T> const &pitch_yaw) {
-	auto[pitch, yaw] = pitch_yaw;
-	return { std::cos(yaw) * std::cos(pitch), std::sin(pitch),
-		std::sin(yaw) * std::cos(pitch) };
+	auto [pitch, yaw] = pitch_yaw;
+	return {std::cos(yaw) * std::cos(pitch), std::sin(pitch),
+			std::sin(yaw) * std::cos(pitch)};
 }
